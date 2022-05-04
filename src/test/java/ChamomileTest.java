43d7b09d82a6a54fa2539a9905918f7f035fd448
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-04 18:57:28 +0200

    Add test Chamomile

---
+++ src/test/java/ChamomileTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChamomileTest {
+
+    @Test
+    public void test1() {
+        assertEquals("I love you", Chamomile.howMuchILoveYou(1));
+        assertEquals("a little", Chamomile.howMuchILoveYou(2));
+        assertEquals("not at all", Chamomile.howMuchILoveYou(6));
+        assertEquals("a little", Chamomile.howMuchILoveYou(20));
+    }
+}
