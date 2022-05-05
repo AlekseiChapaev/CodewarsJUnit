f602ee84bd4031f34ef24781d7e290a7931c4928
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-05 14:17:55 +0200

    Add test TransportationOnVacation

--- src/main/java/switchItUp/SwitchItUp.java
+++ src/main/java/switchItUp/SwitchItUp.java
@@ -1,4 +1,15 @@
 package switchItUp;
+/**
+ * https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+ *
+ * When provided with a number between 0-9, return it in words.
+ *
+ * Input :: 1
+ *
+ * Output :: "One".
+ *
+ * If your language supports it, try using a switch statement.
+ */
 
 public class SwitchItUp {
     public static String switchItUp(int number){
