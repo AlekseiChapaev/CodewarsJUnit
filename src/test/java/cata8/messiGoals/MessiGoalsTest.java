36bdfe968eb2f2b45c813ffed42dc73e023b618d
Aleksei Chapaev <alekseiachapaev@gmail.com>
2022-05-08 12:11:50 +0200

    Add MessiGoals Test

---
+++ src/test/java/cata8/messiGoals/MessiGoalsTest.java
@@ -0,0 +1,13 @@
+package cata8.messiGoals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessiGoalsTest {
+
+    @Test
+    public void BasicTests() {
+        assertEquals(58, MessiGoals.totalGoals);
+    }
+}
